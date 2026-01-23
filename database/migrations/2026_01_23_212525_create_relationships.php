610cc6e4b2e06d952b0bf70ea0132f4a57138177
Nicolas <kochhann.nicolass.soldi@gmail.com>
2026-01-23 19:19:14 -0300

    Relationships migration fix

--- database/migrations/2026_01_23_212525_create_relationships.php
+++ database/migrations/2026_01_23_212525_create_relationships.php
@@ -12,17 +12,32 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create("users_projects", function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('project_id');
+            $table->enum('role', ['owner', 'leader', 'member', 'observer']);
+            $table->timestamps();
+
+            $table->primary(['user_id', 'project_id']);
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
-            $table->enum('role', ['owner', 'leader', 'member', 'observer']);
         });
 
         Schema::create('projects_features', function (Blueprint $table) {
+            $table->unsignedBigInteger('project_id');
+            $table->unsignedBigInteger('feature_id');
+            $table->timestamps();
+
+            $table->primary(['project_id', 'feature_id']);
             $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
             $table->foreign('feature_id')->references('id')->on('features')->onDelete('cascade');
         });
 
         Schema::create('features_steps', function (Blueprint $table) {
+            $table->unsignedBigInteger('feature_id');
+            $table->unsignedBigInteger('step_id');
+            $table->timestamps();
+
+            $table->primary(['feature_id', 'step_id']);
             $table->foreign('feature_id')->references('id')->on('features')->onDelete('cascade');
             $table->foreign('step_id')->references('id')->on('steps')->onDelete('cascade');
         });
@@ -33,6 +48,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('relationships');
+        Schema::dropIfExists('user_projects');
+        Schema::dropIfExists('projects_features');
+        Schema::dropIfExists('features_steps');
     }
 };
