b26b0f7f770383b95adb1dc182c47e92cfc2a1c0
Nicolas <kochhann.nicolass.soldi@gmail.com>
2026-02-19 21:33:41 -0300

    Use enum class for feature status default value

--- database/migrations/2026_01_23_211522_create_features_table.php
+++ database/migrations/2026_01_23_211522_create_features_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\FeatureStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,7 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->jsonb('links')->nullable();
-            $table->enum('status', ['pending','in_progress', 'completed'])->default('pending');
+            $table->enum('status', ['pending','in_progress', 'completed'])->default(FeatureStatus::PENDING->value);
             $table->timestamps();
             $table->unsignedBigInteger('project_id');
             $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
